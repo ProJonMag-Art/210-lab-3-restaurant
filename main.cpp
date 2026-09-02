0d74442f84d15c9d36826fe9a614f3a04ed6bfa2
Jonvianney Maglasang <ProJonMag@gmail.com>
2026-09-01 20:01:22 -0700

    2nd Upd: Finished user input function basic functionality, no input validation yet. Added object item outputter, will turn into its own dedicated function.

--- main.cpp
+++ main.cpp
@@ -4,34 +4,53 @@
 
 using namespace std;
 
+// Could use a constructor function, but not required.
 struct Restaurant
 {
     string name;
-    unsigned int menuItemCount;
     int phoneNum;
     double distance;
     float rating;
+    unsigned int menuItemCount;
 };
 
 
 
 int main()
 {
-
+    Restaurant newRest = getUserData();
+    cout << newRest.name << " " << newRest.phoneNum << " " << newRest.distance << " " << newRest.rating << " " << newRest.menuItemCount;
     return 0;
 }
 
+// Gets user inputs and outputs a new restaurant object (Needs input validation)
 Restaurant getUserData()
 {
     string tempStr;
-    unsigned int tempItemCount;
     int tempPhone;
     double tempDist;
     float tempRating;
+    unsigned int tempItemCount;
 
     cout << "Enter Restaurant Name: ";
     cin >> tempStr;
+    cout << endl;
 
+    cout << "Enter Phone Number: ";
+    cin >> tempPhone;
+    cout << endl;
+
+    cout << "Enter Distance: ";
+    cin >> tempDist;
+    cout << endl;
+
+    cout << "Enter Rating: ";
+    cin >> tempRating;
+    cout << endl;
+
+    cout << "Enter Menu Item Count: ";
+    cin >> tempItemCount;
+    cout << endl;
     
-    return Restaurant();
+    return {tempStr, tempPhone, tempDist, tempRating, tempItemCount};
 }
